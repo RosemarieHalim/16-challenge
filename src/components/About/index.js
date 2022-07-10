694691d1512c0595f91060b25e650e19aea882e2
Rosemarie <rosemarie.m.halim@gmail.com>
2022-07-10 19:11:53 -0400

    added about component

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5 aboutMe">
+      <h1 id="about">Where Design Meets Code</h1>
+      <div className="my-2">
+        <p>
+        Hi there! I am Graphic Designer that dabbles in web development
+        from Windsor, Ontario. I believe in strong team work to create 
+        an experience for clients by creating new and unique graphic 
+        solutions. I enjoy all parts of the creative process and learning 
+        new skills. I'm not afraid to take initiative and take on new 
+        challenges to improve myself. My goal is to have fun and spread 
+        new ideas and perspectives around our community.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
