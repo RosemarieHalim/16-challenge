d5819a2f39727439188c3f67443d1f77e25aafe8
Rosemarie <rosemarie.m.halim@gmail.com>
2022-07-10 19:09:49 -0400

    created files for src folder to gather all components into one

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+
+function App() {
+	const [currentTab, setCurrentTab] = useState("about");
+
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+		</div>
+	);
+}
+
+export default App;
